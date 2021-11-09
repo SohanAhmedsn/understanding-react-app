e005da34c6f916345c608ddd258e4ea9a95a3d60
sn77sohan <sohan.ahmedself@gmail.com>
2021-11-09 22:45:11 +0600

    2nd-edition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,209 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello</h2>
     </div>
   );
 }
 
+
+
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function Countries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v2/all")
+    .then(res => res.json())
+    .then(data => setCountries(data))
+  },[])
+  return (
+    <div>
+      <h2>Traveling around the world</h2>
+      <h3>Available Countrie: {countries.length}</h3>
+      {
+        countries.map(country => <Country>
+          name ={country.name} capital={country.capital}
+        </Country>)
+      }
+
+    </div>
+  )
+}
+
+
+function Country(props) {
+  console.log(props);
+  return (
+    <div>
+      <h2>Name: {props.country.name}</h2>
+      <h3>Capital: {props.capital}</h3>
+    </div>
+  )
+}
+ */
